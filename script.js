af6ba8d9b99b4479416a9c96310ae6df5cd6f723
Rhea Phillips <rheaphillips08@gmail.com>
2025-06-02 22:08:07 -0400

    create load event function, start(), for default page settings

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const start = function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.body.style.backgroundColor = '#222';
+};
+
+window.addEventListener('load', start());
